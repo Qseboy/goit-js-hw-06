72a1f1e08bed663d99208b683b075be6795dadfd
Nikolay Shein <qseboy1998@gmail.com>
2023-05-21 19:00:43 +0300

    add 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('span.color');
+const buttonEl = document.querySelector('button.change-color');
+
+const changeColor = event => {
+  console.log(spanEl);
+  let currentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentColor;
+  spanEl.textContent = `${currentColor}`;
+};
+
+buttonEl.addEventListener('click', changeColor);
