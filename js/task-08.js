82bdee69e7bc9bc903bad940d2117d80844af879
Nikolay Shein <qseboy1998@gmail.com>
2023-05-21 18:44:41 +0300

    add 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
+const data = {};
 
+const validateForm = event => {
+  event.preventDefault();
+
+  const { email, password } = event.target.elements;
+
+  if (!email.value || !password.value) {
+    alert('Все поля должны быть заполнены');
+    return;
+  }
+
+  data.email = email.value;
+  data.password = password.value;
+  console.log(data);
+  event.currentTarget.reset(); //reset
+};
+
+formEl.addEventListener('submit', validateForm);
