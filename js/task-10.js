9f0507c135ac1b94f07eeaea1597ea4d7d255774
Nikolay Shein <qseboy1998@gmail.com>
2023-05-22 14:47:26 +0300

    mvp 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,59 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в
+// input и нажимает кнопку Создать, после чего рендерится коллекция.При нажатии на кнопку Очистить, коллекция элементов очищается.
+
+// Создай функцию createBoxes(amount), которая принимает один параметр - число.
+// Функция создает столько < div >, сколько указано в amount и добавляет их в div#boxes.
+
+// Размеры самого первого <div> - 30px на 30px.
+// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+// Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
+
+// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+
+const boxesEl = document.querySelector('#boxes');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const inputCount = document.querySelector('input');
+
+const getValueOfInput = event => {
+  let inputValue = +event.currentTarget.value;
+
+  if (inputValue) {
+    createBtn.addEventListener('click', createBoxes(inputValue));
+  }
+};
+
+const createBoxes = amount => {
+  return () => {
+    console.log('amount' + amount);
+    let widthEl = 30;
+    let heigthEl = 30;
+    for (let i = 0; i < amount; i++) {
+      const divEl = document.createElement('div');
+
+      if (i == 0) {
+        divEl.style.width = `${widthEl}px`;
+        divEl.style.height = `${heigthEl}px`;
+        divEl.style.backgroundColor = getRandomHexColor();
+        boxesEl.append(divEl);
+        continue;
+      }
+
+      widthEl += 10;
+      heigthEl += 10;
+
+      divEl.style.width = `${widthEl}px`;
+      divEl.style.height = `${heigthEl}px`;
+      divEl.style.backgroundColor = getRandomHexColor();
+
+      boxesEl.append(divEl);
+    }
+  };
+};
+
+// get value of input
+inputCount.addEventListener('change', getValueOfInput);
